06f5fb86fcc79421994554a4b8565d0e701dde0b
krystal <krystalkodiyil@gmail.com>
2025-06-30 20:06:48 +0530

    Fix: set base path for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 ADD THIS LINE
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
